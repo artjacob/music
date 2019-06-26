9202c33205a7c464af93ba76c83461b1f180c68c
artjacob <jacob@emitremmus.com>
2019-06-26 01:41:57 -0300

    Projeto básico a partir de template

---
+++ dev/gulp-config.js
@@ -0,0 +1,126 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// gulp / config ///////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+const fs = require("fs-extra");
+
+let config = { };
+
+// CSS
+config["css"] = {
+	"color": "cyan",
+	"dir": "./source/css/",
+	"watch": ["**/**.scss"],
+	"source": ["./source/css/music.scss"],
+	"destination": {
+		"development": "./public/"
+	}
+};
+
+// JS
+config["js"] = {
+	"color": "yellow",
+	"dir": "./source/js/",
+	"watch": ["**/**.js"],
+	"source": fs.readJsonSync("./source/js/modules.json"),
+	"destination": {
+		"development": "./public/"
+	}
+};
+
+// HTML
+config["html"] = {
+	"color": "magenta",
+	"dir": "./source/html/",
+	"watch": ["**/**.pug", "**/**.html"],
+	"source": ["./source/html/music.pug"],
+	"destination": {
+		"development": "./public/",
+		"production": "./deploy/"
+	}
+};
+
+// Assets
+config["assets"] = {
+	"color": "green",
+	"dir": "./source/assets/",
+	"watch": ["**"],
+	"files": [
+		// Service Worker e Manifest
+		{
+			"title": "service-worker",
+			"type": "js",
+			"source": "./source/assets/service-worker.js",
+			"destination": {
+				"development": "./public/",
+				"production": "./deploy/"
+			},
+			"filename": "service-worker.js"
+		},
+		{
+			"title": "manifest",
+			"type": "json",
+			"source": "./source/assets/manifest.json",
+			"destination": {
+				"development": "./public/",
+				"production": "./deploy/"
+			},
+			"filename": "manifest.json"
+		},
+
+		// Ícone
+		{
+			"title": "icon",
+			"type": "png",
+			"source": "./source/assets/icon.png",
+			"destination": {
+				"development": "./public/",
+				"production": "./deploy/"
+			},
+			"filename": "icon.png"
+		},
+
+		// Fontes
+		{
+			"title": "cabin-400",
+			"type": "font",
+			"source": "./source/assets/cabin-400.woff2",
+			"destination": {
+				"development": "./public/",
+				"production": "./deploy/"
+			},
+			"filename": "cabin-400.woff2"
+		},
+		{
+			"title": "cabin-500",
+			"type": "font",
+			"source": "./source/assets/cabin-500.woff2",
+			"destination": {
+				"development": "./public/",
+				"production": "./deploy/"
+			},
+			"filename": "cabin-500.woff2"
+		},
+		{
+			"title": "cabin-700",
+			"type": "font",
+			"source": "./source/assets/cabin-700.woff2",
+			"destination": {
+				"development": "./public/",
+				"production": "./deploy/"
+			},
+			"filename": "cabin-700.woff2"
+		},
+		{
+			"title": "material-icons",
+			"type": "font",
+			"source": "./source/assets/material-icons.woff2",
+			"destination": {
+				"development": "./public/",
+				"production": "./deploy/"
+			},
+			"filename": "material-icons.woff2"
+		}
+	]
+};
+
+module.exports = config;
---
+++ dev/gulp-js.js
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// gulp / css //////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+const gulp = require("gulp");
+const browserSync = require("browser-sync").get("gulp");
+const log = require("fancy-log");
+const color = require("ansi-colors");
+const plumber = require("gulp-plumber");
+const sourcemaps = require("gulp-sourcemaps");
+const config = require("./gulp-config.js");
+
+// Módulos específicos para JS
+const concat = require("gulp-concat");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+
+let tasks = { };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Watch
+tasks["watch"] = function watchJS(done) {
+    gulp.watch(config["js"]["watch"], { "cwd": config["js"]["dir"] }, tasks["stage"]);
+    done();
+};
+
+// Stage
+tasks["stage"] = function stageJS(done) {
+    gulp.src(config["js"]["source"])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+
+        .pipe(concat("music.js"))
+        .pipe(babel({ presets: ["@babel/env"] }))
+
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config["js"]["destination"]["development"], { mode: "0644" }));
+
+    log(color.yellow("JS !!"));
+    done();
+};
+
+module.exports = tasks;
