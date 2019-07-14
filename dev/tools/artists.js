e021724e0d5b45a07f071bb769c549a9a0dfab3b
artjacob <jacob@emitremmus.com>
2019-07-14 16:23:06 -0300

    Começando página de artista

--- dev/tools/artists.js
+++ dev/tools/artists.js
@@ -1,16 +1,24 @@
 const fs = require("fs-extra");
 
 let songs = fs.readJsonSync("songs.json");
-let artists = [ ];
+let artists = { };
+let ordered = [ ];
+
+const sortingTitle = (string) => {
+    return string.toLowerCase().replace(/^the /, "");
+};
 
 songs.forEach((song) => {
     let artist = song["artist"];
+    let sorting = sortingTitle(song["artist"]);
 
-    if (!artists.includes(artist)) {
-        artists.push(artist);
+    if (!artists[sorting]) {
+        artists[sorting] = artist;
     }
 });
 
-artists.sort();
+Object.keys(artists).sort().forEach((sorting) => {
+    ordered.push(artists[sorting]);
+});
 
-fs.outputJsonSync("artists.json", artists, { "spaces": "\t" });
+fs.outputJsonSync("artists.json", ordered, { "spaces": "\t" });
