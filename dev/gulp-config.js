2685a1442e83f44bfc7a5a2c5a99aa971d7506a8
artjacob <jacob@emitremmus.com>
2019-07-16 02:40:46 -0300

    Formatação básica do grid de álbuns

--- dev/gulp-config.js
+++ dev/gulp-config.js
@@ -45,81 +45,51 @@ config["assets"] = {
 	"dir": "./source/assets/",
 	"watch": ["**"],
 	"files": [
-		// Service Worker e Manifest
-		{
-			"title": "service-worker",
-			"type": "js",
-			"source": "./source/assets/service-worker.js",
-			"destination": {
-				"development": "./public/",
-				"production": "./deploy/"
-			},
-			"filename": "service-worker.js"
-		},
-		{
-			"title": "manifest",
-			"type": "json",
-			"source": "./source/assets/manifest.json",
-			"destination": {
-				"development": "./public/",
-				"production": "./deploy/"
-			},
-			"filename": "manifest.json"
-		},
+		// // Service Worker e Manifest
+		// {
+		// 	"title": "service-worker",
+		// 	"type": "js",
+		// 	"source": "./source/assets/service-worker.js",
+		// 	"destination": {
+		// 		"development": "./public/",
+		// 		"production": "./deploy/"
+		// 	},
+		// 	"filename": "service-worker.js"
+		// },
+		// {
+		// 	"title": "manifest",
+		// 	"type": "json",
+		// 	"source": "./source/assets/manifest.json",
+		// 	"destination": {
+		// 		"development": "./public/",
+		// 		"production": "./deploy/"
+		// 	},
+		// 	"filename": "manifest.json"
+		// },
 
-		// Ícone
-		{
-			"title": "icon",
-			"type": "png",
-			"source": "./source/assets/icon.png",
-			"destination": {
-				"development": "./public/",
-				"production": "./deploy/"
-			},
-			"filename": "icon.png"
-		},
+		// // Ícone
+		// {
+		// 	"title": "icon",
+		// 	"type": "png",
+		// 	"source": "./source/assets/icon.png",
+		// 	"destination": {
+		// 		"development": "./public/",
+		// 		"production": "./deploy/"
+		// 	},
+		// 	"filename": "icon.png"
+		// },
 
-		// Fontes
-		{
-			"title": "cabin-400",
-			"type": "font",
-			"source": "./source/assets/cabin-400.woff2",
-			"destination": {
-				"development": "./public/",
-				"production": "./deploy/"
-			},
-			"filename": "cabin-400.woff2"
-		},
-		{
-			"title": "cabin-500",
-			"type": "font",
-			"source": "./source/assets/cabin-500.woff2",
-			"destination": {
-				"development": "./public/",
-				"production": "./deploy/"
-			},
-			"filename": "cabin-500.woff2"
-		},
-		{
-			"title": "cabin-700",
-			"type": "font",
-			"source": "./source/assets/cabin-700.woff2",
-			"destination": {
-				"development": "./public/",
-				"production": "./deploy/"
-			},
-			"filename": "cabin-700.woff2"
-		},
-		{
-			"title": "material-icons",
-			"type": "font",
-			"source": "./source/assets/material-icons.woff2",
-			"destination": {
-				"development": "./public/",
-				"production": "./deploy/"
-			},
-			"filename": "material-icons.woff2"
-		}
+		// // Fontes
+		// {
+		// 	"title": "material-icons",
+		// 	"type": "font",
+		// 	"source": "./source/assets/material-icons.woff2",
+		// 	"destination": {
+		// 		"development": "./public/",
+		// 		"production": "./deploy/"
+		// 	},
+		// 	"filename": "material-icons.woff2"
+		// }
 	]
 };
 
