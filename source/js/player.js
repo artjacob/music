de2bb96b30658ea7ff6be29f4b1288c341df2e24
artjacob <jacob@emitremmus.com>
2019-07-18 01:25:19 -0300

    Linha do tempo avançando com a música

--- source/js/player.js
+++ source/js/player.js
@@ -2,20 +2,53 @@
 // player //////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+let $player;
+
+let $np = { };
+
 $(function() {
-    $ui["player"] = $("audio.player")[0];
+    $player = document.querySelector("audio.player");
+
+    $np.position = $(".now-playing .position");
+    $np.length = $(".now-playing .length");
+    $np.elapsed = $(".now-playing .elapsed");
+
+    $player.addEventListener("timeupdate", (event) => {
+        let length = duration($player.duration);
+        $np.length.text(length);
+
+        let position = duration($player.currentTime);
+        $np.position.text(position);
+
+        let percent = $player.currentTime / $player.duration * 100;
+        $np.elapsed.css("width", percent + "%");
+
+        // console.log(position_is_seconds, human_position);
+    });
 
     $ui["now-playing"] = $(".now-playing");
     $(".play", $ui["now-playing"]).on("click", app.Player.toggle);
 });
 
 app.Player = (() => {
+
+    // const updateTimeline
+
     const toggle = () => {
-        if ($ui["player"].paused) {
-            $ui["player"].play();
+        if ($player.paused) {
+            $player.play();
         } else {
-            $ui["player"].pause();
+            $player.pause();
         }
+
+        // console.log("duration", $player.duration);
+        // console.log("volume", $player.volume);
+
+        // console.log("buffered", $player.buffered);
+        // console.log("networkState", $player.networkState);
+        // console.log("played", $player.played);
+        // console.log("readyState", $player.readyState);
+        // console.log("seekable", $player.seekable);
     };
 
     return {
