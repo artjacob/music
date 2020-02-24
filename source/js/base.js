2dc543008a65f5c4163fcfb7df61b329c7d35f42
artjacob <jacob@emitremmus.com>
2020-02-24 04:00:40 -0300

    Firebase configuration

--- source/js/base.js
+++ source/js/base.js
@@ -2,6 +2,29 @@
 // base ////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Firebase
+const firebaseConfig = {
+    "apiKey": "AIzaSyAUvAYQn3Ydv1QzlcX4NIfk5YBb3KJkab4",
+    "authDomain": "player-e985a.firebaseapp.com",
+    "databaseURL": "https://player-e985a.firebaseio.com",
+    "projectId": "player-e985a",
+    "storageBucket": "player-e985a.appspot.com",
+    "messagingSenderId": "70806897195",
+    "appId": "1:70806897195:web:a79dbcc1affa29c458206e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Authentication
+let user;
+
+firebase.auth().onAuthStateChanged((response) => {
+    user = response;
+    if (!user) window.location = "/";
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 let app = { };
 
 let ui = { };
